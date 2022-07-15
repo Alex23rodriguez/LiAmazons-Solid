cc624b667a9f0074136c1e6484c186f581417fc2
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-07-14 23:50:15 -0500

    fix: load default fen

--- src/amazons/singleplayer.ts
+++ src/amazons/singleplayer.ts
@@ -1,26 +1,24 @@
-import { Amazons } from "amazons-game-logic";
+import { Amazons, DEFAULT_POSITIONS } from "amazons-game-logic";
 import type { FEN, Move } from "amazons-game-logic/dist/types";
 import { Game } from "boardgame.io";
 
-function Load(fen: FEN) {
-  let amazons = new Amazons(fen);
-  return amazons;
-}
-
-let amazons = new Amazons("w1/2/2/1b w - 1");
+let amazons = new Amazons(DEFAULT_POSITIONS[8]);
+// FOR DEBUGGING ONLY
+(window as any).amazons = amazons;
 
 export const AmazonsGame: Game = {
   name: "amazons",
 
-  setup: () => ({ fen: amazons.fen(), ascii: amazons.ascii() }),
+  setup: () => ({ fen: amazons.fen() }),
 
   moves: {
     move: (G: any, ctx: any, m: Move) => {
-      const amazons = Load(G.fen);
+      // const amazons = Load(G.fen);
       if (
         (amazons.turn == "w" && ctx.currentPlayer == "1") ||
         (amazons.turn == "b" && ctx.currentPlayer == "0")
       ) {
+        console.log("wrong player");
         return { ...G };
       }
       amazons.move(m);
