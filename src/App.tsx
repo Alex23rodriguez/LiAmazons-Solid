3257b217ec89498f73596fbeeb16e80cf69b4a69
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-07-25 19:43:21 -0500

    feat: basic app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,68 @@
-import type { Component } from "solid-js";
-import { Client } from "boardgame.io/client";
-import { AmazonsGame } from "./amazons/singleplayer";
+import Singleplayer from "./amazons/singleplayer";
+
+export const App = () => <Singleplayer />;
+// import { Client } from "boardgame.io/client";
+// import { AmazonsGame } from "./amazons/game";
 
 // import logo from "./logo.svg";
 // import styles from "./App.module.css";
 
-class AmazonsClient {
-  public client;
-  constructor() {
-    this.client = Client({ game: AmazonsGame, debug: true });
-    this.client.start();
-  }
-}
-
-const app = new AmazonsClient();
-
-// const App: Component = () => {
-//   return (
-//     <div class={styles.App}>
-//       <header class={styles.header}>
-//         {/* <img src={logo} class={styles.logo} alt="logo" /> */}
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           class={styles.link}
-//           href="https://github.com/solidjs/solid"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn Solid
-//         </a>
-//       </header>
-//     </div>
-//   );
-// };
+// class AmazonsClient {
+//   public client;
+//   constructor() {
+//     this.client = Client({ game: AmazonsGame, debug: true });
+//     this.client.start();
+//   }
+// }
 //
-// export default App;
+// const app = new AmazonsClient();
+
+// import { Router } from "solid-app-router";
+// import _ from "lodash";
+// import LiNavLink from "./li-navlink";
+
+// import routes from "./routes";
+// import "./app.css";
+
+// CSS for the sidebar is taken from vue.css
+// export const App = () => (
+//   <Router>
+//     <main>
+//       <aside className="sidebar">
+//         <div className="sidebar-nav" style={{ height: "90%" }}>
+//           <ul>
+//             {routes.map((route_category, idx) => (
+//               <li key={idx}>
+//                 <p>{route_category.name}</p>
+//                 <ul>
+//                   {route_category.routes.map((route, _idx) => (
+//                     <LiNavLink
+//                       key={`${idx}.${_idx}`}
+//                       to={route.path}
+//                       exact={true}
+//                       activeClassName="active"
+//                     >
+//                       {route.text}
+//                     </LiNavLink>
+//                   ))}
+//                 </ul>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       </aside>
+//       <section className="content">
+//         {_.flattenDeep(routes.map((route) => route.routes)).map(
+//           (route, idx) => (
+//             <Route
+//               key={idx}
+//               exact
+//               path={route.path}
+//               component={route.component}
+//             />
+//           )
+//         )}
+//       </section>
+//     </main>
+//   </Router>
+// );
