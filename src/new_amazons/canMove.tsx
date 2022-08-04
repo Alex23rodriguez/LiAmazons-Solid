0de4029762fea1deaafeb5f534ec2cf07819417f
Alejandro Rodriguez <alex.rodriguez.oro@gmail.com>
2022-08-03 19:14:20 -0500

    feat: small style additions to canMove

--- src/new_amazons/canMove.tsx
+++ src/new_amazons/canMove.tsx
@@ -1,11 +1,20 @@
 import { Square as TSquare } from "amazons-game-engine/dist/types";
 import { Component } from "solid-js";
-export const CanMove: Component<{ onClick: (sq: TSquare) => void }> = (
-  props
-) => {
+import { colorPalette } from "./settings";
+export const CanMove: Component<{
+  shooting: boolean;
+  onClick: (sq: TSquare) => void;
+}> = (props) => {
   return (
     <div class="absolute grid place-items-center w-full h-full">
-      <div class="rounded-full w-1/4 h-1/4 bg-red-400 z-10"></div>
+      <div
+        class="rounded-full w-1/4 h-1/4 z-10"
+        style={{
+          opacity: 0.5,
+          "background-color":
+            colorPalette()[props.shooting ? "canMoveShooting" : "canMove"],
+        }}
+      ></div>
     </div>
   );
 };
--- src/new_amazons/settings.ts
+++ src/new_amazons/settings.ts
@@ -8,6 +8,8 @@ export const [colorPalette, setColorPalette] = createSignal<{
     dark: "#16a34a",
     w: "white",
     b: "slateblue",
+    canMove: "black",
+    canMoveShooting: "red",
     arrow: "black",
     light_h: "yellow",
     dark_h: "yellow",
